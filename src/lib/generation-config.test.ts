76c96d36816f56b1f44e2220acc8ee393eb1ea9d
thonyAGP <thony56.gtr@gmail.com>
2026-02-23 07:56:07 +0000

    test: add skeleton for src/lib/generation-config.ts

---
+++ src/lib/generation-config.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+// TODO: import from 'src/lib/generation-config';
+
+describe('generation-config', () => {
+  it.todo('should test OUTPUT_MODES');
+  it.todo('should test GRID_OPTIONS');
+  it.todo('should test OutputMode');
+  it.todo('should test GridLayout');
+});
