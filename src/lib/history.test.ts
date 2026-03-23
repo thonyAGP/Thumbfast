662f921a493db282205898204fa84baef5e02597
thonyAGP <thony56.gtr@gmail.com>
2026-03-23 08:03:25 +0000

    test: add skeleton for src/lib/history.ts

---
+++ src/lib/history.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+// TODO: import from 'src/lib/history';
+
+describe('history', () => {
+  it.todo('should test historyDB');
+  it.todo('should test HistoryImage');
+  it.todo('should test HistorySettings');
+  it.todo('should test HistoryEntry');
+});
