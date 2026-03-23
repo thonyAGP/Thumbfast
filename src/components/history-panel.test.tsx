a4fc5453ea433b5d9e78e342019adce9f19eb771
thonyAGP <thony56.gtr@gmail.com>
2026-03-23 08:03:28 +0000

    test: add skeleton for src/components/history-panel.tsx

---
+++ src/components/history-panel.test.tsx
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest';
+// TODO: import from 'src/components/history-panel';
+
+describe('history-panel', () => {
+  it.todo('should test HistoryPanel');
+});
