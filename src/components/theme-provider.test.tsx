805153866e2d83c06df53567ab91122656be8e1d
thonyAGP <thony56.gtr@gmail.com>
2026-03-23 08:03:29 +0000

    test: add skeleton for src/components/theme-provider.tsx

---
+++ src/components/theme-provider.test.tsx
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest';
+// TODO: import from 'src/components/theme-provider';
+
+describe('theme-provider', () => {
+  it.todo('should test ThemeProvider');
+});
